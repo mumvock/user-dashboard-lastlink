b59d94ad37695cc729fa57b6c2db93671212385a
Mumvock <mumvock@pm.me>
2025-11-03 05:13:07 -0300

    chore: limpeza e reconfiguração do componente principal

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,10 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.scss'
+    selector: 'body[app-root]',
+    imports: [RouterOutlet],
+    template: '<router-outlet />'
 })
 export class App {
-  protected readonly title = signal('user-dashboard');
 }
